dc8046a2a89055bace3dac2abb88a91b13ad062d
Paulo Moutinho <paulo_vicali@icloud.com>
2022-07-09 20:59:16 -0300

    create image github and avatar

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,33 +1,26 @@
+import { useEffect, useState } from 'react'
 import '../styles/table.scss'
 
 export function Table() {
+    const [ repositories, setRepositories ] = useState<string[]>([])
+
+    useEffect(() => {fetch('https://api.github.com/users/moutinhofuturedev/repos')
+    .then((response) => response.json()).then((data) => {
+        const repositoiesNames = data.map((item: { name: any }) => item.name)
+
+        setRepositories(repositoiesNames)
+    })
+}, [])
+
     return(
-        <form>
-            <table>
-                <tr>
-                    <th>Name</th>
-                    <th>Job</th>
-                </tr>
-                <tbody>
-                    <tr>
-                        <td>Paulo</td>
-                        <td>React Developer JR</td>
-                    </tr>
-                    <tr>
-                        <td>Daniel</td>
-                        <td>Full Stack PL</td>
-                    </tr>
-                    <tr>
-                        <td>Michelle</td>
-                        <td>React Developer JR</td>
-                    </tr>
-                    <tr>
-                        <td>Mayk</td>
-                        <td>Full Stack SR</td>
-                    </tr>
-                </tbody>
-            </table>
-        </form>
+        <div className='box'>
+            <img src="https://www.github.com/moutinhofuturedev.png" alt="" />
+            <strong>Paulo Moutinho</strong>
+            <p>Frontend Software Enginner</p>
+            <ol>
+                {repositories.map((repo) => (<li key={repo}>{repo}</li>))}
+            </ol>
+        </div>
     )
 }
 
