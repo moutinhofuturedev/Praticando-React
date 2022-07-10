f4843af7a1933efcb3fbf88aa292b3488461b6e5
Paulo Moutinho <paulo_vicali@icloud.com>
2022-07-09 21:00:01 -0300

    update component Table

--- src/pages/Catalog.tsx
+++ src/pages/Catalog.tsx
@@ -6,15 +6,20 @@ import '../styles/catalog.scss'
 
 export function Catalog() {
     return(
-        <div className="catalog">
-            <h1>React Catalog</h1>
-            <p>Sereval examples of React</p>
+        <div className='main'>
+            <div className="catalog">
+                <h1>Github Catalog</h1>
+                <p>Sereval examples of React</p>
 
-            <div>
-                <ReactUI text='Catalog'/>
-                <p>Go to <Link to="/">App</Link> </p>
+                <div>
+                    <ReactUI text='Catalog' />
+                    <p>Go to <Link to="/">App</Link> </p>
+                </div>
+
             </div>
-            <Table/>
+            <section>
+                <Table />
+            </section>
         </div>
     )
 }
