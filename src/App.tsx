a1495f4cfb982ab6dccea1038540a2ac69a80c5d
Paulo Moutinho <paulo_vicali@icloud.com>
2022-07-14 11:48:28 -0300

    update in component app

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,16 @@ import { ReactUI } from './components/ReactUI'
 import reactImg from './assets/react_icon.svg'
 import './styles/app.scss'
 
-import { useState } from 'react'
+import { FormEvent, useState } from 'react'
 import { Link } from 'react-router-dom'
 
 function App() { // um componente é uma função que retorna um html
-  const [ counter, setCounter ] = useState<string[]>(['React']) // o valor inicial do estado é um array 
+  const [ counter, setCounter ] = useState<string[]>([]) // o valor inicial do estado é um array 
+  const [ value, setValue ] =useState('')
 
-  function createReactUI() {
-    setCounter([...counter,'React'])
+  function createReactUI(event: FormEvent) {
+    event.preventDefault()
+    setCounter([...counter, value])
   }
 
   return(
@@ -21,12 +23,13 @@ function App() { // um componente é uma função que retorna um html
         <p>A Javascript library for building user interfaces</p>
       </aside>
       <main>
-        <div>
+        <form onSubmit={createReactUI}>
           {counter.map(reacts => {
             return <ReactUI text={reacts} />
           })}
-          <button onClick={createReactUI}>Add React</button>
-        </div>
+          <button type='submit'>Add React</button>
+          <input type="text" onChange={event => setValue(event.target.value)}/>
+        </form>
 
         <p>Go to <Link to="/catalog">Catalog</Link> </p>
       </main>
